0f3ff83bb044aead420873502b5bef951abca2a3
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-11-18 22:41:47 +0000

    v1.0
    Release NoSnap!

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using SG.Airlock;
+
+namespace NoSnap;
+
+[BepInPlugin("uk.hotten.nosnap", "NoSnap", "1.0")]
+public class Plugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new Harmony("uk.hotten.nosnap");
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+
+        Log.LogInfo($"NoSnap is ready!");
+    }
+
+    [HarmonyPatch(typeof(MinigameEngageZone), "EnterSnapZone")]
+    public static class SnapPatch
+    {
+        public static void Prefix(MinigameEngageZone __instance)
+        {
+            __instance.UseMinigameSnap = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NoSnap
+A mod for Among Us VR that stops your character's head from snapping to another direction when entering a task.
+
+## Installation
+ - Install [BepInEx Bleeding Edge](https://builds.bepinex.dev/projects/bepinex_be) into the Among Us VR installation folder.
+ - Drag `NoSnap.dll` into the `BepInEx/plugins` folder.
